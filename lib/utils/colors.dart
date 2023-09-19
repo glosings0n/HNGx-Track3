6604c561eaf4252e473e96ca6bad849f1f18f04c
KellyDanielO <kizdver@gmail.com>
2023-09-19 21:12:47 +0100

    chore: Added some last min colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -10,14 +10,10 @@ class AppColors {
    static const Color searchGray = Color(0xFFF2F2F2);
    static const Color backgroundColor = Color(0xFFFFFFFF);
    static const Color buttonColor = Color(0xFF0085FF);
-   static const Color primaryColor = Color(0xFF0085FF);
-   static const Color color = Color(0xFFF2F2F2);
-   static const Color shadeTextColor = Color(0xFF595959);
-   static const Color boxShade = Color(0xFFF2F2F2);
-   static const Color commentShade = Color(0xFF8C8C8C);
-   static const Color commentBackground = Color(0xFFF2F2F2);
+
+  // Added a last minuteColor
+  static const Color tShadeColor = Color(0xFFA6A6A6);
+  static const Color tWhite = Color(0xFFFFFFFF);
+
 
 }
-
-
-
