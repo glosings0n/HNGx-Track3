1744437b62a3d16bc42e8dbf2a13755f40c7c9ce
KellyDanielO <kizdver@gmail.com>
2023-09-19 20:45:43 +0100

    fix: A path error

--- lib/widgets/comment_widget.dart
+++ lib/widgets/comment_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:free_lunch_app/utils/colors.dart';
+import '../utils/colors.dart';
 
 class CommentWidget extends StatefulWidget {
   final String initialCommentText;
