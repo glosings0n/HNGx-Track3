71025176731843ffa28051a70c34a16e8a76b228
MrazTevin <millatevin@gmail.com>
2023-09-19 11:07:57 +0300

    chore(colors): update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -10,8 +10,10 @@ class AppColors {
    static const Color searchGray = Color(0xFFF2F2F2);
    static const Color backgroundColor = Color(0xFFFFFFFF);
    static const Color buttonColor = Color(0xFF0085FF);
-
-
+   static const Color primaryColor = Color(0xFF0085FF);
+   static const Color color = Color(0xFFF2F2F2);
+   static const Color shadeTextColor = Color(0xFF595959);
+   static const Color boxShade = Color(0xFFF2F2F2);
 
 }
 
