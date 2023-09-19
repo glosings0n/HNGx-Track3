7594466c99676adf487c1d1fef8ec7a78fbb9650
MrazTevin <millatevin@gmail.com>
2023-09-19 03:41:56 +0300

    style: set color scheme for project
    This commit set  the color scheme used in the project to improve visual aesthetics.
    colors have been selected and applied to enhance the user interface.

---
+++ lib/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+   static const  Color tPrimaryColor = Color(0xFF0085FF);
+   static const  Color tPrimaryColor1 = Color(0xFFE5F3FF); // use this for the burger icon color to the right of the welcome text
+   static const  Color tAmberAccent  = Color(0xFFffab07);
+   static const  Color tAmberAccent1  = Color(0xFFffa800);
+   static const  Color tBlack = Color(0xFF252525);
+   static const Color tGray = Color(0xFFD9D9D9);
+   static const Color searchGray = Color(0xFFF2F2F2);
+   static const Color backgroundColor = Color(0xFFFFFFFF);
+   static const Color buttonColor = Color(0xFF0085FF);
+
+
+
+}
+
+
+
