203bb5dd9a16961905f9dbbdb042cb97bc640aec
KellyDanielO <kizdver@gmail.com>
2023-09-20 20:01:07 +0100

    chore: displaying the widgets

--- lib/widgets_test_screen.dart
+++ lib/widgets_test_screen.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'utils/colors.dart';
@@ -7,6 +7,9 @@ import 'utils/svg_icons.dart';
 import 'widgets/account_info_withdraw_summary.dart';
 import 'widgets/action_buttons.dart';
 import 'widgets/avatar.dart';
+import 'widgets/custom_badge.dart';
+import 'widgets/custom_card.dart';
+import 'widgets/select_image.dart';
 import 'widgets/total_card.dart';
 
 class WidgetsTestScreen extends StatefulWidget {
@@ -53,10 +56,70 @@ class _WidgetsTestScreenState extends State<WidgetsTestScreen> {
               const Text('Lunch Recived'),
               AppSvgIcons.lunchRecieved,
               const Text('Mini Action Button'),
+              const Text('Select Cam'),
+              SizedBox(height: height * .02),
+              const SelectCam(
+                height: 56,
+                width: 56,
+                icon: Icon(
+                  CupertinoIcons.camera,
+                  color: AppColors.tBlack4,
+                ),
+              ),
+              SizedBox(height: height * .02),
+              const Text('Custom Cards'),
+              SizedBox(height: height * .02),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomCard(
+                    cardText: '1',
+                    icon: AppSvgIcons.hamburgerDark,
+                    iconHeight: 18,
+                    iconWidth: 18,
+                  ),
+                  SizedBox(width: width * .04),
+                  CustomCard(cardText: '2', icon: AppSvgIcons.hamburgerDark),
+                  SizedBox(width: width * .04),
+                  CustomCard(cardText: '3', icon: AppSvgIcons.hamburgerDark),
+                  SizedBox(width: width * .04),
+                  CustomCard(cardText: '4', icon: AppSvgIcons.hamburgerDark),
+                ],
+              ),
+              SizedBox(height: height * .02),
+              const Text('Bage Icons'),
+              CustomBadgeIcon(
+                icon: const Icon(
+                  Icons.check,
+                  color: AppColors.tPrimaryColor,
+                ),
+                filledColor: AppColors.tPrimaryColor.withOpacity(.2),
+              ),
+              SizedBox(height: height * .02),
+              const CustomBadgeIcon(
+                icon: Icon(
+                  Icons.person_remove_alt_1_outlined,
+                  color: AppColors.tPrimaryColor,
+                ),
+                outlineColor: AppColors.tPrimaryColor,
+              ),
+              SizedBox(height: height * .02),
+              MiniActionBtn(
+                text: 'Lunch',
+                btnColor: AppColors.tPrimaryColor,
+                icon: AppSvgIcons.hamburgerLight,
+              ),
+              SizedBox(height: height * .02),
               MiniActionBtn(
                 text: 'Send Lunch',
                 btnColor: AppColors.tPrimaryColor,
                 icon: AppSvgIcons.hamburgerLight,
+              ),
+              SizedBox(height: height * .02),
+              const MiniOutlinedActionBtn(
+                text: 'Upload image',
+                btnColor: AppColors.tPrimaryColor,
+                textColor: AppColors.tPrimaryColor,
               ),
               const Text('Action Button 1'),
               ActionBtn(
@@ -107,6 +170,16 @@ class _WidgetsTestScreenState extends State<WidgetsTestScreen> {
                 totalNum: '12',
                 width: width * .9,
                 height: height * .2,
+                text1: 'You\'ve done well this month, Cheers 🥂',
+                text2: 'Free Lunches',
+              ),
+              SizedBox(height: height * .02),
+              TotalCardThree(
+                totalNum: '500',
+                width: width * .9,
+                height: height * .2,
+                text1: 'You\'ve',
+                text2: 'Free Lunches',
               ),
               SizedBox(height: height * .02),
               WithdrawSummary(
