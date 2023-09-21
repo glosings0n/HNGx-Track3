cfba9601725b1ed2ea6a11ad7fd81fcc7b26cef1
G-Losingson <georgesbyona@gmail.com>
2023-09-21 23:47:40 +0200

    differents changement in reusable components file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'feature/screens/lunch.screen.dart';
+import 'feature/screens/send.request.screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SendLunches(totalLunches: '12',),
+      home: SendRequestPage(),
+      // home: SendLunches(totalLunches: '12',),
     );
   }
 }
